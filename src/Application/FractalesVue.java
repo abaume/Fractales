fb500b1493c007a849c2725ad628989dc7d566e2
Lapicardise Romain <romain.lapicardise@etu.u-bordeaux.fr>
2017-11-23 18:23:47 +0100

    cercle

--- src/Application/FractalesVue.java
+++ src/Application/FractalesVue.java
@@ -2,9 +2,15 @@ package Application;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JComponent;
 import javax.swing.*;
 
@@ -14,6 +20,7 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 	private FractalesControleur controleur;
 	private FractalesModèle model;
 
+	private Ellipse2D circle = new Ellipse2D.Double(102.57, 26.8, 42.8, 42.8);
 
 	@Override
 	public void update(Observable o, Object arg) {
@@ -23,8 +30,9 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 	}
 
 	public FractalesVue(FractalesControleur controleur, FractalesModèle modele) {
+		super();
 		this.controleur = controleur;
-		this.model = modele;
+		this.model = modele;		
 	}
 
 	public void actionPerformed(ActionEvent e) {}
@@ -58,7 +66,7 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 		float image_x = (x2 - x1) * zoom;
 		float image_y = (y2 - y1) * zoom;
 
-
+		System.out.println("je suis presque la");
 
 		for (int x = 0; x < image_x; x++) {
 			for (int y = 0; y < image_y; y++) {
@@ -67,7 +75,6 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 					System.out.println("je suis la");
 					// dessiner le point
 					g.fillOval(x, y, 1,1);
-
 					g.fillOval(1, 1, 10, 10);
 				}
 			}
@@ -78,23 +85,31 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		System.out.println("je suis exécutée");
-		g.fillOval(10, 10, 2,2);
+		Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setPaint(Color.darkGray);
+        g2d.fill(circle);
+        g2d.dispose();
 	}
 	
 
 	public static void main(String[] args) {
 
-		new Fenetre("Fractales");
+		//new Fenetre("Fractales");
 
 		FractalesModèle model = new FractalesModèle();
 		FractalesControleur controller = new FractalesControleur(model);
-		
-//		view.afficherMandelBrot(null);
-		
 		FractalesVue view = new FractalesVue(controller, model);
-		Dessin g = new Dessin();
+		//Dessin g = new Dessin();		
+		
+		JFrame f = new JFrame();
+        f.add(view);
+        f.setSize(400, 300);
+        
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        f.setVisible(true);
 		
 		model.addObserver(view);
-		view.afficherMandelBrot(g);
+		//view.afficherMandelBrot(g);
 	}
 }

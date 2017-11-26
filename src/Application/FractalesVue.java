e0e75d41ba2689d88b06ff4f7db8fb577ba1297f
abaume <baume.aurore@gmail.com>
2017-11-26 17:25:47 +0100

    FRACTAAAAALE

--- src/Application/FractalesVue.java
+++ src/Application/FractalesVue.java
@@ -39,10 +39,10 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 	//			
 	//			}
 
-/*
- * @author baume
- * @
- */
+	/*
+	 * @author baume
+	 * @
+	 */
 	public void paint(Graphics g) {
 		// définition de la zone à dessiner sachant que l'ensemble
 		// de Mandelbrot est toujours compris entre les coordonnées suivantes
@@ -54,18 +54,18 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 		float y1 = model.gety1();
 		model.sety2((float)1.2);
 		float y2 = model.gety2();
-//		model.setx1(0); 
-//		float x1 = model.getx1();
-//		model.setx2(500);
-//		float x2 = model.getx2();
-//		model.sety1(0);
-//		float y1 = model.gety1();
-//		model.sety2(500);
-//		float y2 = model.gety2();
-//		
+		//		model.setx1(0); 
+		//		float x1 = model.getx1();
+		//		model.setx2(500);
+		//		float x2 = model.getx2();
+		//		model.sety1(0);
+		//		float y1 = model.gety1();
+		//		model.sety2(500);
+		//		float y2 = model.gety2();
+		//		
 		System.out.println("je suis paintComponent");
 
-//		// on utilise le paint de la classe JFrame
+		//		// on utilise le paint de la classe JFrame
 		super.paint(g);
 
 		float zoom = model.getZoom();
@@ -84,29 +84,12 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 				if ( i == iteration_max) {
 					System.out.println("je suis la");
 					// dessiner le point
-					g.fillOval(x, y, 1, 1);
+					g.fillRect(x, y, 1, 1);
 					this.repaint();
 				}
 			}
 		}
 	}
-
-	@Override
-	/*
-	 * (non-Javadoc)
-	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
-	 */
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		System.out.println("je suis exécutée");
-		Graphics2D g2d = (Graphics2D) g.create();
-//        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setPaint(Color.darkGray);
-        g2d.fill(circle);
-        g2d.dispose();
-        
-	}
-	
 
 	public static void main(String[] args) {
 
@@ -116,14 +99,14 @@ public class FractalesVue extends JComponent implements Observer, ActionListener
 		FractalesControleur controller = new FractalesControleur(model);
 		FractalesVue view = new FractalesVue(controller, model);
 		//Dessin g = new Dessin();		
-		
+
 		JFrame f = new JFrame();
-        f.add(view);
-        f.setSize(400, 300);
-        
-        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        f.setVisible(true);
-		
+		f.add(view);
+		f.setSize(400, 300);
+
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		f.setVisible(true);
+
 		model.addObserver(view);
 		//view.afficherMandelBrot(g);
 	}

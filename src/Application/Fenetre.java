ded7360021f82a69cfbefd9fccddfb006fb9f211
Lapicardise Romain <romain.lapicardise@etu.u-bordeaux.fr>
2017-11-28 09:27:02 +0100

    bouton (pas encore au point)

--- src/Application/Fenetre.java
+++ src/Application/Fenetre.java
@@ -5,6 +5,7 @@ package Application;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,6 +19,8 @@ import javax.swing.JPanel;
  */
 public class Fenetre extends JFrame implements ActionListener{
 
+	private JPanel pan = new JPanel();
+	private JButton bouton = new JButton("Mon bouton");
 	/**
 	 * 
 	 */
@@ -28,9 +31,27 @@ public class Fenetre extends JFrame implements ActionListener{
 	 * @category constructeur
 	 */
 	public Fenetre (String titre) {
+		super();
 		this.setBounds(10, 0, 961, 880);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);		
-		this.setVisible(true);
+		this.setTitle("Fractales");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);	
+		//setContentPane(buildContentPane());
+	    this.setVisible(true);
+	}
+	
+	private JPanel buildContentPane(){
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		
+		JButton bouton = new JButton("Cliquez ici !");
+		bouton.setBounds(961, 0, 30, 30);
+		panel.add(bouton);
+		
+		JButton bouton2 = new JButton("Ou là !");
+		panel.add(bouton2);
+		bouton2.setBounds(961, 200, 30, 30);
+		
+		return panel;
 	}
 
 	@Override
